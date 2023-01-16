a3e58d0582d3feaf06194bbc5f829fbb486afffb
Marco Sousa <marcosousacontato@gmail.com>
2023-01-16 12:26:36 -0300

    Criando classe PosicaoXadrez e método para fazer a conversão

--- Xadrez-Csharp/Program.cs
+++ Xadrez-Csharp/Program.cs
@@ -8,20 +8,8 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.ColocarPeca(new Torre(Cor.PRETA, tab), new Posicao(2, 4));
-                tab.ColocarPeca(new Torre(Cor.PRETA, tab), new Posicao(2, 5));
-                tab.ColocarPeca(new Torre(Cor.PRETA, tab), new Posicao(2, 6));
-                tab.ColocarPeca(new Rei(Cor.PRETA, tab), new Posicao(2, 3));
-                tab.ColocarPeca(new Torre(Cor.PRETA, tab), new Posicao(9, 9));
-                Tela.ImprimeTabuleiro(tab);
-            } 
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos.PosicaoXadrezParaMatriz());
         }
     }
 }
